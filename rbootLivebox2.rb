df865c8d3fba45a0fc52a7b8efb7a4ce90b7086d
Pierrot de la Lune <pierre@ile-australe.eu>
2010-10-07 22:25:52 +0200

    - Initial commit

---
+++ rbootLivebox2.rb
@@ -0,0 +1,30 @@
+require "rubygems"
+require "celerity"
+require "highline/import"
+
+abort $0 + " requires JRuby" unless defined?(RUBY_ENGINE) && RUBY_ENGINE == "jruby"
+
+browser = Celerity::Browser.new
+browser.secure_ssl=false
+browser.goto('https://juandenova.ile-australe.eu:12345/')
+
+login=ask("login:    ")
+password=ask("password: ") { |q| q.echo = "x"}
+
+browser.text_field(:name => "authlogin").value=login
+browser.text_field(:name => "authpasswd").value=password
+browser.link(:title => "Accéder").click
+
+print "Connexion reussie\n" if browser.link(:text => "Déconnexion").exists?
+browser.li(:text => "Configuration").click 
+
+print "Onglet de configuration OK\n" if browser.cell(:text => "Dépannage & Maintenance").exists?
+browser.cell(:text => "Dépannage & Maintenance").click
+
+print "Page Dépannage & Maintenance ok\n" if browser.link(:text => "Redémarrer").exists? 
+browser.link(:id => "linkbutt1").click
+
+print "Print confirmation de redemarrage OK\n" if browser.cell(:text => "Oui").exists?
+#browser.link(:text => "Oui").click
+#
+#print "Redemarrage en cours\n" if browser.contains_text("Veuillez patienter")
