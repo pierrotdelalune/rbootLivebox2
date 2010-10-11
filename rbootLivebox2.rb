fc2de0fd02c93b3dbd615000c4ff001373c4a9eb
Pierrot de la Lune <pierre@ile-australe.eu>
2010-10-11 21:44:50 +0200

    - Various improvements   + The URL is now a command line argument   + The is now an on line help   + new argument --dryrun to go through each step of the scenario without restarting the Livebox

--- rbootLivebox2.rb
+++ rbootLivebox2.rb
@@ -1,13 +1,53 @@
+# == Synopsis
+#
+# rbootLivebox2: Celerity scenario to automatically reboot a Sagem Livebox 2 by Orange
+#
+# == Usage
+#
+# rbootLivebox2 [OPTION] ... URL
+#
+# -h, --help:
+#    show help
+#
+# --dryrun:
+#    go through each step of the scenario but does not reboot the Livebox 2
+#
+# URL: The URL of the Livebox 2
+
 require "rubygems"
 require "celerity"
 require "highline/import"
+require "getoptlong"
+require "rdoc/usage"
 
 abort $0 + " requires JRuby" unless defined?(RUBY_ENGINE) && RUBY_ENGINE == "jruby"
 
+opts = GetoptLong.new(
+         [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+         [ '--dryrun', GetoptLong::NO_ARGUMENT ]
+         )
+
+dryrun=false
+opts.each do |opt, arg|
+   case opt
+      when '--help'
+        RDoc::usage
+      when '--dryrun'
+         dryrun=true
+   end
+end
+
+if ARGV.length != 1
+   puts "Missing url argument (try --help)"
+   exit 1
+end
+
+url=ARGV.shift
+
 browser = Celerity::Browser.new
+# The SSL certificate is self signed and hostname in certificate does match hostname in url
 browser.secure_ssl=false
 
-url=ask("url:    ")
 browser.goto(url)
 
 login=ask("login:    ")
@@ -27,6 +67,9 @@ print "Page Dépannage & Maintenance ok\n" if browser.link(:text => "Redémarrer
 browser.link(:id => "linkbutt1").click
 
 print "Print confirmation de redemarrage OK\n" if browser.cell(:text => "Oui").exists?
-#browser.link(:text => "Oui").click
-#
-#print "Redemarrage en cours\n" if browser.contains_text("Veuillez patienter")
+
+unless dryrun
+   browser.link(:text => "Oui").click
+
+   print "Redemarrage en cours\n" if browser.contains_text("Veuillez patienter")
+end
